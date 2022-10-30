d124488efcc6283a7339eeb4982b30c387515ceb
Pavel Shirokiy <pshirokiy@gmail.com>
2022-10-30 17:53:08 +0300

    Initial Commit

---
+++ UIKit_Slider_in_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIKit_Slider_in_SwiftUI
+//
+//  Created by 19543442 on 30.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIKit_Slider_in_SwiftUI/UIKit_Slider_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_Slider_in_SwiftUIApp.swift
+//  UIKit_Slider_in_SwiftUI
+//
+//  Created by 19543442 on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_Slider_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
